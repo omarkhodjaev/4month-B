a6ff26fa0411a81ad062227949d380bcbff740cc
omarkhodjaev <umarkhankhodjaev.com>
2022-11-07 05:20:48 +0500

    delete method added

--- script/app.js
+++ script/app.js
@@ -1,3 +1,4 @@
+'use strict'
 const containerEl = document.querySelector(".container");
 
 (() => {
@@ -10,14 +11,29 @@ const containerEl = document.querySelector(".container");
     function render(data){
     data.map(({image,price,title,id}) => {
         const divEl = document.createElement("div");
-        divEl.className = "user-card"
+        divEl.className = "user-card";
         divEl.innerHTML = `
         <i data-user-id="${id}" class="fas fa-trash"></i>
         <img src="${image}" width="200px" height="200px" /> 
         <h5>Price: ${price}</h5>
         <h5>Title: ${title}</h5>
-
         `
         containerEl.appendChild(divEl)
     })
   }
+
+    containerEl.addEventListener('click', (e) => {
+        if(e.target.matches("i")){
+            const userId = e.target.dataset.userId 
+            fetch(`https://fakestoreapi.com/products/${userId}`,
+            {
+                method: "DELETE"
+            }
+            )
+            .then(response => {
+                if(response.status == 204){
+                    alert("salom");
+                }
+            });
+        }
+    })
